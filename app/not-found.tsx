0ebd755128562d4c84f103164655a37c44900fe2
정대윤 <sensecodevalue@gmail.com>
2024-09-02 00:12:12 +0900

    fix: add container component in not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 
+import { Container } from "@/components/Container";
+
 export default function NotFound() {
   return (
-    <div>
+    <Container as="div">
       <h2>Not Found</h2>
       <p>Could not find requested resource</p>
       <Link href="/">Return Home</Link>
-    </div>
+    </Container>
   );
 }
